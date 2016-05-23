25a43a4cbce59644bdac38f6a4fec69c5ca0350d
jargote <javier@booodl.com>
2016-05-23 20:43:17 +1000

    Implemented Offer.isElegible method. It determines how many times an offer can be applied to a shopping cart.

--- src/main/scala/com/hipstershop/model/Offer.scala
+++ src/main/scala/com/hipstershop/model/Offer.scala
@@ -13,4 +13,30 @@ import org.joda.time.DateTime
  * @param endDate, When this offer expires.
  */
 case class Offer(name: String, items: Map[Product, Int], price: Double,
-                 startDate: DateTime, endDate: DateTime)
+                 startDate: DateTime = DateTime.now(),
+                 endDate: DateTime = DateTime.now().plusDays(1)) {
+
+  /**
+   * Determines whether an offer could be applied or not to the current
+   * shopping cart.
+   *
+   * @param shoppingCart, products with their respective quantities
+   * @param times, how many times this offer could be applied
+   * @return Whether this offer applies to items in the shopping cart (T/F) and
+   *         updated shopping cart if offer applies, unmodified otherwise
+   */
+  def isEligible(shoppingCart: Map[Product, Int], times: Int = 0): (Map[Product, Int], Int) = {
+    val hasAllItems = (
+      items.foldLeft(Seq[Boolean]()) {
+        case (valid: Seq[Boolean], (prod: Product, qty: Int)) =>
+          valid :+ ((shoppingCart.getOrElse(prod, 0) - qty) >= 0)
+      }).forall(x => x)
+
+    if (hasAllItems) {
+      isEligible(items.foldLeft(shoppingCart) {
+        case (cart: Map[Product, Int], (product: Product, qty)) =>
+          cart.updated(product, cart.getOrElse(product, 0) - qty)
+      }, times + 1)
+    } else (shoppingCart, times)
+  }
+}
