57c66f1e0d2486ba28e8255bae22f0b0280da6db
jargote <javier@booodl.com>
2016-05-23 21:16:22 +1000

    renamed class attribute cart -> shoppingCart.

--- src/main/scala/com/hipstershop/model/Transaction.scala
+++ src/main/scala/com/hipstershop/model/Transaction.scala
@@ -5,10 +5,10 @@ import org.joda.time.DateTime
 /**
  * This class represents a Transaction (Shopping Cart).
  *
- * @param cart
+ * @param shopppingCart
  * @param date
  * @param offers
  */
-case class Transaction(cart: Map[Product, Int], date: DateTime = DateTime.now(),
+case class Transaction(shoppingCart: Map[Product, Int], date: DateTime = DateTime.now(),
                        offers: List[Offer] = List()) {
 }
