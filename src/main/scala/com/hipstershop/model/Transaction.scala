d970c92db052e37a644771b32501e9f9bf1f70a7
jargote <javier@booodl.com>
2016-05-23 21:17:31 +1000

    Implemented Transaction.getTotal method. This method calculates the total a customer must pay for all its items after offers.

--- src/main/scala/com/hipstershop/model/Transaction.scala
+++ src/main/scala/com/hipstershop/model/Transaction.scala
@@ -11,4 +11,25 @@ import org.joda.time.DateTime
  */
 case class Transaction(shoppingCart: Map[Product, Int], date: DateTime = DateTime.now(),
                        offers: List[Offer] = List()) {
+
+  /**
+   * Calculates the total amount to pay for this products.
+   *
+   * @return purchase total
+   */
+  def getTotal: Double = {
+    val (updatedCart: Map[Product, Int], oTotal: Double) = {
+      offers.foldLeft((shoppingCart, 0.0)) {
+        case ((cart: Map[Product, Int], oTotal: Double), offer: Offer) =>
+          val (updatedCart, oTimes) = offer.isEligible(cart)
+
+          (updatedCart, oTotal + offer.price * oTimes)
+      }
+    }
+
+    // Calculating total for remiaining items in shopping cart.
+    oTotal + updatedCart.foldLeft(0.0) {
+      case (acc: Double, (prod: Product, qty: Int)) => acc + (prod.price * qty)
+    }
+  }
 }
