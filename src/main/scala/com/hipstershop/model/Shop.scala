73c837a335d46ea5743830f200c4e7b14b524f46
jargote <javier@booodl.com>
2016-05-23 21:38:44 +1000

    Implemented Shop.updateStock. This method adds a new product to stock and sets it's stocks levels to the specified value if it's a new stock item, otherwise it would update the existing stock value by applying a + / - operation.

--- src/main/scala/com/hipstershop/model/Shop.scala
+++ src/main/scala/com/hipstershop/model/Shop.scala
@@ -30,4 +30,18 @@ case class Shop(name: String, offers: List[Offer] = List(),
     transactions = transaction :: transactions,
     cashTotal = cashTotal + transaction.getTotal)
 
+  /**
+   * Updates the shop's stock performing the given operation (+/-)
+   * on a current stock value.
+   *
+   * @param product, A Product object instance.
+   * @param quantity, Stock quantity.
+   * @param op, operator used to modify stock quantity.
+   * @return Shop, Immutable Shop object.
+   */
+  def updateStock(product: Product, quantity: Int,
+                  op: (Int, Int) => Int): Shop = {
+    this.copy(stock = stock.updated(product.name,
+      (product, op(stock.getOrElse(product.name, (product, 0))._2, quantity))))
+  }
 }
