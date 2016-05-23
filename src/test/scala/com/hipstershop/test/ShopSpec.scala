d53339ae58157294f5c212646618d74f88de6561
jargote <javier@booodl.com>
2016-05-23 21:25:48 +1000

    Implemented test for Store.addOffer method.

---
+++ src/test/scala/com/hipstershop/test/ShopSpec.scala
@@ -0,0 +1,23 @@
+package com.hipstershop.test
+
+import org.specs2.mutable.Specification
+
+class ShopSpec extends Specification {
+  "addOffer method" should {
+    "add a new shop offer" in new MainTestScope {
+      hipsterShop.addOffer(offerA).offers must contain(offerA)
+    }
+
+    "addTransaction method" should {
+      "add a new transaction" in new MainTestScope {
+        hipsterShop.addTransaction(
+          transactionA).transactions must contain(transactionA)
+      }
+
+      "update cash total" in new MainTestScope {
+        hipsterShop.addTransaction(
+          transactionA).cashTotal must equalTo(0.85)
+      }
+    }
+  }
+}
