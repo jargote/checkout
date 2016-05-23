a243cd9000d416bbcf6a58fac6653e90035a8451
jargote <javier@booodl.com>
2016-05-23 20:02:23 +1000

    Implemented Product model. This class represents a shop item that is available for sale.

---
+++ src/main/scala/com/hipstershop/model/Product.scala
@@ -0,0 +1,9 @@
+package com.hipstershop.model
+
+/**
+ * This class represents a stock Product.
+ *
+ * @param name, product name.
+ * @param price, product price.
+ */
+case class Product(name: String, price: Double)
